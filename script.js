d9d8080204eae1e97ff7942e6fa2cc0d0ea26be1
Aish <kapoor.aishvarya001@gmail.com>
2023-12-02 21:58:35 +0530

    only dont model

--- script.js
+++ script.js
@@ -49,25 +49,34 @@ function staticLoadPlaces() {
     ];
 }
 
+// var models = [
+//     {
+//         url: './assets/magnemite/scene.gltf',
+//         scale: '0.5 0.5 0.5',
+//         info: 'Magnemite, Lv. 5, HP 10/10',
+//         rotation: '0 180 0',
+//     },
+//     {
+//         url: './assets/articuno/scene.gltf',
+//         scale: '0.2 0.2 0.2',
+//         rotation: '0 180 0',
+//         info: 'Articuno, Lv. 80, HP 100/100',
+//     },
+//     {
+//         url: './assets/dragonite/scene.gltf',
+//         scale: '0.08 0.08 0.08',
+//         rotation: '0 180 0',
+//         info: 'Dragonite, Lv. 99, HP 150/150',
+//     }, {
+//         url: './assets/donut/scene.glb',
+//         scale: '0.08 0.08 0.08',
+//         rotation: '0 180 0',
+//         info: 'Donut, Lv. 99, HP 150/150',
+//     },
+// ];
+
 var models = [
     {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
-    {
-        url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
-        info: 'Articuno, Lv. 80, HP 100/100',
-    },
-    {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
-        info: 'Dragonite, Lv. 99, HP 150/150',
-    }, {
         url: './assets/donut/scene.glb',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
