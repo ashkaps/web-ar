a8132b2808a2f59d74c6a52a04474f8b4af7c3ee
Aish <kapoor.aishvarya001@gmail.com>
2023-12-04 01:19:56 +0530

    fix

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ var models = [
     // }
     {
         url: './assets/donut/Donnut1.glb',
-        scale: '0.3 0.3 0.3',
+        scale: '0.1 0.1 0.1',
         rotation: '0 180 0',
         //  position: '0 10 0',
         info: 'Donut',
