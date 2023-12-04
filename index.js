193cf9ecfb1614fd3093ccca5db05568c7a06ee6
Aish <kapoor.aishvarya001@gmail.com>
2023-12-04 12:45:49 +0530

    new

--- index.js
+++
@@ -1,9 +0,0 @@
-import 'arjs/aframe/build/aframe-ar-nft.js'
-
-AFRAME.registerComponent('clicker', {
-    init: function() {
-        this.el.addEventListener('click', e => {
-            alert('Box clicked!');
-        });
-    }
-});
